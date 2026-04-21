96db7402094319272f1f560b1664d295e4fd4b19
FaakhirMemon030 <alexwatson031105@gmail.com>
2026-04-21 08:28:03 +0500

    feat: create Admin mongoose model with password comparison method

--- Backend/models/Admin.js
+++ Backend/models/Admin.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const adminSchema = new mongoose.Schema({
     name: { type: String, default: 'Mymn SaaB' },
@@ -8,17 +7,9 @@ const adminSchema = new mongoose.Schema({
     role: { type: String, default: 'admin' }
 });
 
-// Hash password before saving
-adminSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
-
-// Compare password
+// Plain text comparison
 adminSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+    return enteredPassword === this.password;
 };
 
 module.exports = mongoose.model('Admin', adminSchema);
