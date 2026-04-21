a18b3409ef7f3617d0171a0f503f2d9a52d2f7c3
FaakhirMemon030 <alexwatson031105@gmail.com>
2026-04-21 08:27:37 +0500

    feat: create User mongoose model with schema and password comparison method

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -17,17 +16,9 @@ const userSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now }
 });
 
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
-
-// Compare password
+// Plain text comparison as requested (NOT RECOMMENDED FOR PRODUCTION)
 userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+    return enteredPassword === this.password;
 };
 
 module.exports = mongoose.model('User', userSchema);
